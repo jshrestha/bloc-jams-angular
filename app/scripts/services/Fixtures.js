430fc1bcfef6dac8c350a4c056260fb89dc0b602
Jeevan Shrestha <shresthajeevan@gmail.com>
2017-08-16 16:32:39 -0700

    Assignment 6 Services Part 1 completed

--- app/scripts/services/Fixtures.js
+++ app/scripts/services/Fixtures.js
@@ -34,6 +34,14 @@
      Fixtures.getAlbum = function() {
          return albumPicasso;
      };
+     Fixtures.getCollection = function(numberOfAlbums) {
+            var arr = [];
+            for (var i = 0; i < numberOfAlbums; i++) {
+               arr.push(albumPicasso);
+            }
+            return arr;
+        };
+
          return Fixtures;
      }
 
